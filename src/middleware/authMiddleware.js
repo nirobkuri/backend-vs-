50db5157c21f9e2a5b2541ce7a95d7bcefdef16b
Nirob kuri <nirobkuri448@gmail.com>
2026-02-22 16:12:43 +0000

    middleware

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -4,7 +4,10 @@ const User = require("../models/User");
 
 const protect = asyncHandler(async (req, res, next) => {
   let token;
-  if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
     try {
       token = req.headers.authorization.split(" ")[1];
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
