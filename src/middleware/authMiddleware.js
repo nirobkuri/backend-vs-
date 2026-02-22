f5eee41deb2c3d29d23aeddffbb6c76b3916de23
Nirob kuri <nirobkuri448@gmail.com>
2026-02-22 10:23:18 +0000

    all ok

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -8,17 +8,14 @@ const protect = asyncHandler(async (req, res, next) => {
 
   if (req.headers.authorization?.startsWith("Bearer")) {
     token = req.headers.authorization.split(" ")[1];
-
     try {
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
       req.user = await User.findById(decoded.id).select("-password");
-      return next(); // ✅ token valid, next() call
+      return next(); // ✅ next() শুধু valid token হলে কল
     } catch (error) {
       return res.status(401).json({ message: "Not authorized, token failed" });
     }
-  }
-
-  if (!token) {
+  } else {
     return res.status(401).json({ message: "Not authorized, no token" });
   }
 });
