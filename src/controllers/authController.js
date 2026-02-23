6b8d8f36d9f5726ce090299e0b57040bed4f834c
nirob <51313905-nirobkuri448@users.noreply.replit.com>
2026-02-23 11:17:20 +0000

    all code are fixex now

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,32 +1,23 @@
-const asyncHandler = require("express-async-handler");
 const User = require("../models/User");
 const generateToken = require("../utils/generateToken");
 
-const registerUser = asyncHandler(async (req, res) => {
+const registerUser = async (req, res) => {
   const { name, email, password } = req.body;
 
-  // 1. Check if all fields are provided
   if (!name || !email || !password) {
     res.status(400);
     throw new Error("Please include all fields");
   }
 
-  // 2. Check if user already exists
   const userExists = await User.findOne({ email });
   if (userExists) {
     res.status(400);
     throw new Error("User already exists");
   }
 
-  // 3. Create the user
-  const user = await User.create({
-    name,
-    email,
-    password,
-  });
+  const user = await User.create({ name, email, password });
 
   if (user) {
-    // 4. Send success response with token
     res.status(201).json({
       _id: user._id,
       name: user.name,
@@ -38,11 +29,12 @@ const registerUser = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("Invalid user data");
   }
-});
+};
 
-const loginUser = asyncHandler(async (req, res) => {
+const loginUser = async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
+
   if (user && (await user.matchPassword(password))) {
     res.json({
       _id: user._id,
@@ -57,13 +49,11 @@ const loginUser = asyncHandler(async (req, res) => {
     res.status(401);
     throw new Error("Invalid email or password");
   }
-});
+};
 
-const getMe = asyncHandler(async (req, res) => {
+const getMe = async (req, res) => {
   const user = await User.findById(req.user._id).select("-password");
   res.json(user);
-});
+};
 
 module.exports = { registerUser, loginUser, getMe };
-
-
